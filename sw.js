3666f7cdbbb232390e5e4bdf95f56b4705512889
rhpgfccwvr-dev <rhpgfccwvr@privaterelay.appleid.com>
2026-08-16 01:15:27 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE="master-communication-v1";
+const FILES=["./","./index.html","./manifest.json"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));
+self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
+self.addEventListener("fetch",e=>{
+  if(e.request.method!=="GET") return;
+  e.respondWith(caches.match(e.request).then(x=>x||fetch(e.request).then(r=>{
+    const copy=r.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return r;
+  }).catch(()=>caches.match("./index.html"))));
+});
